f4f42d0cbbe3f54601c8e094730774e4c34f1d60
clb <buntulevycaleb@gmail.com>
2025-04-15 22:43:41 +0200

    created a employee of Generic class

--- src/main/java/com/example/employeemanagement/HelloController.java
+++ src/main/java/com/example/employeemanagement/HelloController.java
@@ -1,14 +1,25 @@
 package com.example.employeemanagement;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void goToEmployeeManagement() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("employee-view.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Employee Management");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
